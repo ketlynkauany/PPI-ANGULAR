bb32594220f7c410d38a9c5debf50ca20d58f632
ketlynkauany <ketlynlove13@gmail.com>
2026-05-12 01:10:38 -0300

    002

--- primeiro-app/src/app/exibe-mensagem/exibe-mensagem.ts
+++ primeiro-app/src/app/exibe-mensagem/exibe-mensagem.ts
@@ -4,31 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-exibe-mensagem',
   standalone: true,
   imports: [],
-  template: `
-    <form>
-      <label for="nome">Digite seu nome</label>
-
-      <input
-        id="nome"
-        name="nome"
-        type="text"
-        #nome
-        (keyup)="alterarMensagem(nome.value)"
-      >
-    </form>
-
-    <div class="msg">
-      <p>{{mensagem}}</p>
-    </div>
-  `,
-  styles: [`
-    div.msg {
-      background-color: #CED8F6;
-      border-radius: 5px;
-      margin-top: 10px;
-      padding: 10px;
-    }
-  `]
+  templateUrl: './exibe-mensagem.html',
+  styleUrl: './exibe-mensagem.css'
 })
 
 export class ExibeMensagem {
